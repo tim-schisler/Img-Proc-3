b249ceade43416abd02b9e695758f7c5d1cc75e4
Tim Schisler <theschis@gmail.com>
2018-12-11 02:05:45 -0600

    Assignment resubmission: Q5 fix
    Fixes the compilation error by removing the call to cv::selectROI,
    which was implemented in OpenCV 3.x but is not available on the
    school server running OpenCV 2.4.5.  Instead, the fix constructs a
    cv::Rect structure based on command-line arguments and uses it to
    select a portion of the image.

--- q5.cpp
+++ q5.cpp
@@ -20,32 +20,56 @@ Assignment 3: Image Enhancement in Spatial Domain
 const char * DISPLAY = "Histogram Equalized ROI";
 
 int main(int argc, char *argv[]) {
-
+  
   //Command line argument processing
-  if(argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " imgfile" << std::endl;
+  if(argc != 6) {
+    std::cerr << "Usage: " << argv[0] << " imgfile x y width height" << std::endl;
     return -1;
   }
-
-  //Open image from command line argument.
-  cv::Mat img = cv::imread ( argv[1], cv::IMREAD_GRAYSCALE );
+  
+  cv::Mat img = cv::imread ( argv[1], cv::IMREAD_ANYCOLOR );
   if ( img.empty() ) {
     std::cerr << "Unable to open picture file " << argv[1] << std::endl;
     return -1;
   }
+  
+  int x = atoi(argv[2]),
+      y = atoi(argv[3]),
+      width = atoi(argv[4]),
+      height = atoi(argv[5]);
+  if(x >= img.cols || y >= img.rows) {
+    std::cerr << "ROI start point out of bounds" << std::endl
+	    << argv[1] << ": " << img.cols << "x" << img.rows << std::endl;
+	return -1;
+  }
+  if(img.cols - x < width) width = img.cols - x;
+  if(img.rows - y < height) height = img.rows - y;
+
 
   //Select the region of interest and apply histogram equalization to it.
-  cv::Rect roiRect = cv::selectROI("Select ROI with Mouse", img);
+  cv::Rect roiRect(x, y, width, height);
   std::vector<cv::Mat> roiMat;
-  roiMat[0] = img(roiRect);
+  roiMat.push_back( img(roiRect) );
   int c;
   histEq(&roiMat[0], &roiMat, &c);
-
+  if (c > 1) {
+    cv::merge(roiMat, roiMat[0]);
+  }
   //Replace the ROI in the image with its histogram-equalized counterpart.
-  for (int rw = roiRect.y; rw < roiRect.y + roiRect.height; rw++)
-    for (int col = roiRect.x; col < roiRect.x + roiRect.width; col++)
-      img.at<uchar>(rw,col) = roiMat[0].at<uchar>(rw-roiRect.y, col-roiRect.x);
-
+  for (int rw = roiRect.y; rw < roiRect.y + roiRect.height; rw++) {
+    for (int col = roiRect.x; col < roiRect.x + roiRect.width; col++) {
+	  //Outline the border of the ROI.
+	  if(rw == roiRect.y 
+      || rw == roiRect.y + roiRect.height - 1 
+      || col == roiRect.x 
+	  || col == roiRect.x + roiRect.width - 1) {
+		img.at<cv::Vec3b>(rw,col) = {0,0,0};
+	  }
+	  else {
+	    img.at<cv::Vec3b>(rw,col) = roiMat[0].at<cv::Vec3b>(rw-roiRect.y, col-roiRect.x);
+	  }
+    }
+  }
   //Display the image with ROI replaced.
   cv::namedWindow ( DISPLAY );
   cv::imshow ( DISPLAY, img );
